d864a2493ae9e72327c91900d6f225b75a48f424
guido miranda <gmirce@gmail.com>
2022-06-03 19:17:38 -0400

    Docs

---
+++ docs/precache-manifest.630d1c86ee55c96f3cd9f146b14eaa2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44546b5b3196766cfc2f5609bd4d7801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78d0524da7e481a5cb88",
+    "url": "/static/css/main.ec80207a.chunk.css"
+  },
+  {
+    "revision": "74a1d13579bddac993c0",
+    "url": "/static/js/2.15a841a7.chunk.js"
+  },
+  {
+    "revision": "78d0524da7e481a5cb88",
+    "url": "/static/js/main.f30e559d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "71cbd69b2893e006874140d6f077c5fb",
+    "url": "/static/media/logo.71cbd69b.jpg"
+  },
+  {
+    "revision": "caca2e554d04edcd1e2395d27923860d",
+    "url": "/static/media/portada.caca2e55.jpg"
+  }
+]);
